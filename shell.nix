ba74b70bb17e1a94f1873eaaa865974744030c6a
opusdelu24 <supanat.d5118208@gmail.com>
2025-06-28 07:54:56 +0700

    my first commit

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  pythonPackages = with pkgs.python3Packages; [
+    jupyterlab
+    notebook
+    ipykernel
+    pip
+    numpy
+    pandas
+    matplotlib
+  ];
+in
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    #nodejss
+  ] ++ pythonPackages;
+
+  shellHook = ''
+    echo "wellcum 0w0"
+    echo "Run 'jupyter notebook' or 'jupyter lab' to get started 💻"
+  '';
+}
+
